1227204d05c98b2881faa797b993a15d6cead105
varbogas <varbogas@gmail.com>
2023-10-02 15:09:22 -0700

    pin gsap

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".square", {
-    x: 700,
-    duration: 3,
+    // x: 1000,
+    duration: 8,
     scrollTrigger: {
         trigger: ".square",
         start: "top 80%",
         end: "top 30%",
         scrub: 4,
         toggleActions: "restart none none none",
+        pin: ".square",
+        pinSpacing: true,
         markers: {
             startColor: "purple",
             endColor: "fuchsia",
