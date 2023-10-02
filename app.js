95f04bc70e762eead46b416656ace7518fa729a7
varbogas <varbogas@gmail.com>
2023-10-02 14:10:14 -0700

    third gsap scroll lesson

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.to(".square", {
-    // x: 700,
+    x: 700,
     duration: 3,
     scrollTrigger: {
         trigger: ".square",
-        start: "top 30%",
-        end: () => `+=${document.querySelector(".square").offsetHeight}`,
-        markers: true,
-        toggleClass: "red"
+        start: "top 60%",
+        end: "top 40%",
+        toggleActions: "restart pause resume complete",
+        // play pause zoom resume reverse restart reset complete none
+                      //onEnter onLeave onEnterBack onLeaveBack
+        markers: {
+            startColor: "purple",
+            endColor: "fuchsia",
+            fontSize: "3rem",
+        },
     }
 })
